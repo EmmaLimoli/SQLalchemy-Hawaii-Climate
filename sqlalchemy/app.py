fd50cf5d4e293a02083205364a875f76dded500b
Emma Limoli <EmmaLimoli024@gmail.com>
2020-08-16 13:55:15 -0400

    start flask

---
+++ sqlalchemy/app.py
@@ -0,0 +1,71 @@
+#need to join the station/measurement tabels for some of the queries
+#flask jsonify to convert API data into json response object
+#dependencies 
+import numpy as np
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, and_
+
+from flask import Flask, jsonify
+
+#database setup
+engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
+
+#reflect an existing database into new model
+Base = automap_base()
+
+#reflect the tables
+Base.prepare(engine, reflect=True)
+
+#save references to the table
+
+Measurement = Base.classes.measurement
+
+Station = Base.classes.station
+
+#Flask setup
+app = Flask(__name__)
+
+#Flask Routes
+
+@app.route("/")
+def welcome():
+    """All available api routes."""
+    return(
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<end>/<end><br/>"
+    )
+#precipitation, convert query results to a dict using date as the key and prcp as value
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+
+    #return json rep of jsonify
+
+#stations, return jsonify list of stations from dataset
+@app.route("/api/v1.0/stations")
+def stations():
+
+#tobs, query dates/tobs of most active station for last year of data
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+    #return JSON list of tobs for prev year
+
+#return JSON list of min, avg, max temp for given start-end range
+#given start calc TMIN, TAVG, TMAX for dates greater than/equal to start date
+@app.route("/api/v1.0/<start>")
+def 
+
+#given start/end date, calc TMIN, TAVG, TMAX for dates between start and end dates
+@app.route("/api/v1.0/<start>/<end>")
+
+#app.run debug to run 
+if __name__=='__main__':
+    app.run(debug=True)
