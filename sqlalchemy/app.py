fa5472545f26465414a378958504c80e73212c8b
Emma Limoli <EmmaLimoli024@gmail.com>
2020-08-17 09:37:35 -0400

    work on dates

--- sqlalchemy/app.py
+++ sqlalchemy/app.py
@@ -39,8 +39,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<end>/<end><br/>"
+        f"/api/v1.0/yyyy-mm-dd<br/>"
+        f"/api/v1.0/yyyy-mm-dd/yyyy-mm-dd<br/>"
     )
 #precipitation, convert query results to a dict using date as the key and prcp as value
 @app.route("/api/v1.0/precipitation")
@@ -68,20 +68,86 @@ def precipitation():
 #stations, return jsonify list of stations from dataset
 @app.route("/api/v1.0/stations")
 def stations():
+    session = Session(engine)
+   
+    stations = {}
+    all_stations = session.query(Station.station, Station.name).all() 
+    session.close()
+    
+    for st, name in all_stations:
+       stations[st] = name
+    return jsonify(stations)
+
 #calling the entire dateset of the station, calling on the session open and close, process data send it to the web
+
 # #tobs, query dates/tobs of most active station for last year of data
-# @app.route("/api/v1.0/tobs")
-# def tobs():
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    last_12_months_station = session.query(Measurement.date, Measurement.tobs).\
+    filter(Measurement.station == 'USC00519281').\
+    filter(Measurement.date > '2016-08-22').\
+    order_by(Measurement.station).all()
+    session.close()
+
+    all_tobs = []
+    for date, tobs in last_12_months_station:
+        tobs_dict = {}
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+        all_tobs.append(tobs_dict)
+    return jsonify(all_tobs)
+
 
 #     #return JSON list of tobs for prev year
 
 # #return JSON list of min, avg, max temp for given start-end range
 # #given start calc TMIN, TAVG, TMAX for dates greater than/equal to start date
-# @app.route("/api/v1.0/<start>")
-# def 
+@app.route("/api/v1.0/<start>")
+def start(start):
+    session = Session(engine)
+    sel = [Measurement.station,
+    func.max(Measurement.tobs),
+    func.avg(Measurement.tobs),
+    func.min(Measurement.tobs)]
+    tobs_breakdown = session.query(*sel).\
+    group_by(Measurement.station).\
+    order_by(func.count(Measurement.station).desc()).all()
+    session.close()
+
+    tobs_results = []
+    for date, min, avg, max in tobs_breakdown:
+        results_dict = {}
+        results_dict["date"] = date
+        results_dict["min"] = min
+        results_dict["avg"] = avg
+        results_dict["max"] = max
+        tobs_results.append(results_dict)
+    return jsonify(tobs_results)
+
 
 # #given start/end date, calc TMIN, TAVG, TMAX for dates between start and end dates
-# @app.route("/api/v1.0/<start>/<end>")
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start_end):
+    session = Session(engine)
+    sel = [Measurement.station,
+    func.max(Measurement.tobs),
+    func.avg(Measurement.tobs),
+    func.min(Measurement.tobs)]
+    tobs_breakdown = session.query(*sel).\
+    group_by(Measurement.station).\
+    order_by(func.count(Measurement.station).desc()).all()
+    session.close()
+
+    tobs_end = []
+    for date, min, avg, max, in tobs_breakdown:
+        end_dict = {}
+        end_dict["date"] = date
+        end_dict["min"] = min
+        end_dict["avg"] = avg
+        end_dict["max"] = max
+        tobs_end.append(end_dict)
+    return jsonify(tbs_end)
 
 #app.run debug to run 
 if __name__=='__main__':
